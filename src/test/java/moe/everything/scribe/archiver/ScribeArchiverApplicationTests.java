76b949f746f53689c8a66154ee5410e8ee0af263
Barinade <poco0317@users.noreply.github.com>
2021-06-11 20:06:51 -0500

    init

---
+++ src/test/java/moe/everything/scribe/archiver/ScribeArchiverApplicationTests.java
@@ -0,0 +1,13 @@
+package moe.everything.scribe.archiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribeArchiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
